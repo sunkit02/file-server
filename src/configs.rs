874b4a7e7a55122ebc7c3b65ed96e6009840aadd
Sun Kit Tsui <sunkittsui@gmail.com>
2023-11-06 14:36:20 -0600

    refactor: Extract into file_server and configs modules

---
+++ src/configs.rs
@@ -0,0 +1,145 @@
+use clap::{self, arg, command, value_parser};
+
+use std::path::PathBuf;
+
+#[derive(Debug, Clone)]
+pub struct ServerConfigs {
+    pub base_dir: PathBuf,
+    pub host: String,
+    pub port: u16,
+    pub log_level: log::Level,
+}
+
+impl ServerConfigs {
+    pub fn builder() -> ServerConfigsBuilder {
+        ServerConfigsBuilder {
+            base_dir: None,
+            host: None,
+            port: None,
+            log_level: None,
+        }
+    }
+
+    pub fn from_cli_args() -> Self {
+        let matches = command!()
+            .arg(
+                arg!([base_dir] "Optional base directory to serve")
+                    .required(false)
+                    .value_parser(value_parser!(PathBuf)),
+            )
+            .arg(
+                arg!(-p --port <PORT> "Sets custom port")
+                    .required(false)
+                    .value_parser(value_parser!(u16)),
+            )
+            .arg(
+                arg!(-H --host <HOST> "Sets custom host")
+                    .required(false)
+                    .value_parser(value_parser!(String)),
+            )
+            .arg(
+                arg!(-l --loglevel <LOGLEVEL> "Sets log level")
+                    .required(false)
+                    .value_parser(value_parser!(String)),
+            )
+            .get_matches();
+
+        let mut configs_builder = Self::builder();
+
+        if let Some(base_dir) = matches.get_one::<PathBuf>("base_dir") {
+            if !base_dir.exists() {
+                println!("Error: {:?} does not exist.", base_dir);
+                std::process::exit(1);
+            }
+
+            if !base_dir.is_dir() {
+                println!("Error: base_dir is expected to be a directory.");
+                println!("{:?} is not a directory.", base_dir);
+                std::process::exit(2);
+            }
+
+            configs_builder.base_dir(base_dir);
+        }
+
+        if let Some(&port) = matches.get_one::<u16>("port") {
+            configs_builder.port(port);
+        }
+
+        if let Some(host) = matches.get_one::<String>("host") {
+            configs_builder.host(host);
+        }
+
+        if let Some(log_level) = matches.get_one::<String>("loglevel") {
+            configs_builder.log_level(log_level);
+        }
+
+        configs_builder.build()
+    }
+}
+
+impl Default for ServerConfigs {
+    fn default() -> Self {
+        Self {
+            base_dir: PathBuf::from("./"),
+            host: "127.0.0.1".to_string(),
+            port: 8080,
+            log_level: log::Level::Info,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ServerConfigsBuilder {
+    base_dir: Option<PathBuf>,
+    host: Option<String>,
+    port: Option<u16>,
+    log_level: Option<log::Level>,
+}
+
+impl ServerConfigsBuilder {
+    pub fn base_dir(&mut self, path: &PathBuf) -> &Self {
+        self.base_dir = Some(path.to_owned());
+        self
+    }
+
+    pub fn host(&mut self, host: &str) -> &Self {
+        self.host = Some(host.to_string());
+        self
+    }
+
+    pub fn port(&mut self, port: u16) -> &Self {
+        self.port = Some(port);
+        self
+    }
+
+    pub fn log_level(&mut self, log_level: &str) -> &Self {
+        let log_level = match log_level {
+            "error" => log::Level::Error,
+            "warn" => log::Level::Warn,
+            "debug" => log::Level::Debug,
+            "trace" => log::Level::Trace,
+            _ => log::Level::Info,
+        };
+        self.log_level = Some(log_level);
+        self
+    }
+
+    pub fn build(mut self) -> ServerConfigs {
+        let mut config = ServerConfigs::default();
+
+        if let Some(base_path) = self.base_dir.take() {
+            config.base_dir = base_path;
+        }
+        if let Some(host) = self.host.take() {
+            config.host = host;
+        }
+        if let Some(port) = self.port.take() {
+            config.port = port;
+        }
+        if let Some(log_level) = self.log_level.take() {
+            config.log_level = log_level;
+        }
+
+        config
+    }
+}
