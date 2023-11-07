bbc99a875cea3e9fab5f129c9949495a7e74ef69
Sun Kit Tsui <sunkittsui@gmail.com>
2023-11-07 16:29:32 -0600

    refactor: Move directory_structure handler to file_server module

--- src/file_manager/mod.rs
+++ src/file_manager/mod.rs
@@ -1,12 +1,10 @@
 use actix_web::web;
 
 pub mod handlers;
-pub mod models;
 pub mod templates;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
     cfg
         .service(handlers::home_page)
-        .service(handlers::favicon)
-        .service(handlers::dir_structure);
+        .service(handlers::favicon);
 }
--- src/file_server/mod.rs
+++ src/file_server/mod.rs
@@ -1,7 +1,10 @@
 use actix_web::web;
 
 pub mod handlers;
+pub mod models;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(handlers::serve_static_file);
+    cfg
+        .service(handlers::serve_static_file)
+        .service(handlers::dir_structure);
 }
